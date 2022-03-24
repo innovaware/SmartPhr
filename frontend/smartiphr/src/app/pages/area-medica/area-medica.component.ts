1fb20fa7f936a0cfc882fafacc9a82420704291c
Marco <m.spina@innovaware.it>
2022-03-24 18:30:08 +0100

    fix area medica

--- frontend/smartiphr/src/app/pages/area-medica/area-medica.component.ts
+++ frontend/smartiphr/src/app/pages/area-medica/area-medica.component.ts
@@ -46,15 +46,17 @@ export class AreaMedicaComponent implements OnInit {
           })
           .afterClosed()
           .subscribe((data: Paziente) => {
-            if (data != undefined || data) {
-              this.pazienti.push(data);
+             if (data != undefined || data) {
+            //   this.pazienti.push(data);
 
-              const index = this.pazienti.indexOf(paziente, 0);
-              if (index > -1) {
-                this.pazienti.splice(index, 1);
-                console.log("Removed item");
-              }
-              this.eventsSubject.next(this.pazienti);
+            //   const index = this.pazienti.indexOf(paziente, 0);
+            //   if (index > -1) {
+            //     this.pazienti.splice(index, 1);
+            //     console.log("Removed item");
+            //   }
+            //   this.eventsSubject.next(this.pazienti);
+            console.log("Area Infermeristica Update Paziente");
+            Paziente.refresh(data, paziente);
             }
           }),
       //css: "mat-raised-button raised-button action-button",
