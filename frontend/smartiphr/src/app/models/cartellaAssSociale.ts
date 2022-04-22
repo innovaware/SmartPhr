422aee2cbbeb94847f721ffbe820b8247993e563
Marco <m.spina@innovaware.it>
2022-04-22 16:27:33 +0200

    fix marco V9.0

--- frontend/smartiphr/src/app/models/cartellaAssSociale.ts
+++ frontend/smartiphr/src/app/models/cartellaAssSociale.ts
@@ -1,5 +1,6 @@
 import { valutazioneSociale } from './valutazioneSociale';
 import { IndiceSocializzazione } from './indiceSocializzazione';
+import { AltroCartellaSociale } from './altroCartellaSociale';
 
 
 export class cartellaAssSociale {
@@ -11,9 +12,10 @@ export class cartellaAssSociale {
     constructor() {
       this.valutazioneSociale = new valutazioneSociale();
       this.indiceSocializzazione = new IndiceSocializzazione();
+      this.altroCartellaSociale = new AltroCartellaSociale();
     }
     
-  
+    altroCartellaSociale? :AltroCartellaSociale;
     valutazioneSociale?: valutazioneSociale;
     indiceSocializzazione?: IndiceSocializzazione;
 
